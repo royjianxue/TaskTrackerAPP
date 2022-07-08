299da01b444f6e1f9eaf508a882858fb6503f2f9
Jian Xue <royjianxue@yahoo.com>
2022-07-08 14:07:36 -0400

    initial automapper created for projectcontroller class

---
+++ TaskTrackerData/Profiles/ProjectProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace TaskTrackerData.Profiles
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<Domain.Project, Common.Contract.Model.ProjectDto>();
+            //CreateMap<Common.Contract.Model.UserForUpdateDto, Domain.User>();
+            CreateMap<Domain.Project, Common.Contract.Model.ProjectDto>();
+        }
+    }
+}
