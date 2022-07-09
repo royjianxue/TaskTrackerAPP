9a1412622d2146396a0d3312bec2841490a6bf9e
Jian Xue <royjianxue@yahoo.com>
2022-07-09 13:43:07 -0400

    creating ProjectDto for httpput

---
+++ Common.Contract/Model/ProjectForUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Common.Contract.Model
+{
+    public class ProjectForUpdateDto : DateTimeDto
+    {
+        public bool StatusCode { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Body { get; set; }
+
+    }
+}
